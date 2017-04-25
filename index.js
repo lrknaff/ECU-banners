4575a1ab272120f982ac729ab9ff9d15202b0b7b
lrknaff <lacey.knaff@gmail.com>
2017-04-25 09:55:26 -0600

    add toggle to view page

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ $('.list').on('click', 'li', function(e) {
 
   $('iframe').attr('src', link)
 });
+
+
+$('.numbers').on('click', function(e) {
+  e.preventDefault();
+
+  let numbersList = $('.numbers-list')
+  let numbersHeader = $('.numbers')
+
+  if( numbersList.attr('id') === 'hide' ) {
+    numbersList.attr('id', 'show');
+    numbersHeader.text('- Numbers:');
+  } else {
+    numbersList.attr('id', 'hide');
+    numbersHeader.text('+ Numbers:');
+  }
+});
+
+
+$('.home').on('click', function(e) {
+  e.preventDefault();
+
+  let homeList = $('.home-list')
+  let homeHeader = $('.home')
+
+  if( homeList.attr('id') === 'hide' ) {
+    homeList.attr('id', 'show');
+    homeHeader.text('- Home:');
+  } else {
+    homeList.attr('id', 'hide');
+    homeHeader.text('+ Home:');
+  }
+});
